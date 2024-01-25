1470088c6887cd554298339c3913415a43a8b0ac
Anthony KALBE <anthony.kalbe@cibeo.fr>
2024-01-25 12:05:59 +0100

    add the github action and starting of the app

---
+++ src/Page/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import './css/page/home.css';
+import Header from '../Component/Header';
+import Menu from '../Component/Menu';
+
+
+const Home = () => {
+    return (
+        <div>
+            <Header/>
+                <div>je test</div>
+            <Menu/>
+        </div>
+    );
+};
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import Home from './Page/Home'
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Rooter, Routes, Route, Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Rooter>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>        
+      </Routes>
+
+    </Rooter>
+    
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
+
+
